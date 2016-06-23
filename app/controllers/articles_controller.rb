87e77400c4d4c0daf12d6ef0d29861337268ed44
mqxerror <snakehardoos@gmail.com>
2016-06-23 15:46:01 +0100

    adding edit and update article

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -4,11 +4,34 @@ class ArticlesController < ApplicationController
 		@article = Article.new
 	end
 
+
+
 	def create
-		#render plain: params[:article].inspect
 		@article = Article.new(article_params)
-		@article.save
-		redirect_to articles_show(@article)
+
+		if @article.save
+			flash[:notice] =  'article was created succsesfuly'
+			redirect_to article_path(@article)
+		else
+			render 'new'
+		end
+	end
+
+	def show
+		@article = Article.find(params[:id])
+	end
+
+	def edit
+		@article = Article.find(params[:id])
+	end
+	def update
+		@article = Article.find(params[:id])
+		if @article.update(article_params)
+			flash[:notice] =  'article was Updated succsesfuly'
+			redirect_to article_path(@article)
+		else
+			render :edit
+		end
 	end
 
 
