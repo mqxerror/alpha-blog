67cee3df22d10d12889a1d8d42ebcdbcc695299a
mqxerror <snakehardoos@gmail.com>
2016-07-02 02:44:58 +0100

    adding validation to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ActiveRecord::Base
-
+	validates :username, presence: true, length: {minimum: 3, maximum: 10}, uniqueness: {case_sensitive: false}
+	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+	validates :email,    presence: true, length: {minimum: 5, maximum: 100}, uniqueness: { case_sensitive: false },
+	format: { with: VALID_EMAIL_REGEX }
 			
 end
