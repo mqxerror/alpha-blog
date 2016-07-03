bef600067a57ce68261298b2fc9947dac9eec190
mqxerror <snakehardoos@gmail.com>
2016-07-03 04:32:11 +0100

    added gravatar and profile

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,5 +6,10 @@ class User < ActiveRecord::Base
 	validates :email,    presence: true, length: {minimum: 5, maximum: 100}, uniqueness: { case_sensitive: false },
 	format: { with: VALID_EMAIL_REGEX }
 	 has_secure_password
+	 include Gravtastic
+  	 gravtastic :secure => true,
+              :filetype => :gif,
+              :size => 180,
+              :default => :wavatar
 			
 end
