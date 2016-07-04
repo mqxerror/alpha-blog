f8869e213cd2f5d6a43a9fff383a9b790851b557
mqxerror <snakehardoos@gmail.com>
2016-07-04 04:06:12 +0100

    adding delete useras admin

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,14 +1,16 @@
 class UsersController < ApplicationController
 	before_action :set_user, only: [:edit, :update, :show]
 	before_action :require_same_user, only: [:edit, :update, :destroy]
+	before_action :require_admin, only: [:destroy]
 	def new
 		@user = User.new
 	end
 	def create
 		@user = User.new(user_params)
 		if @user.save
+			session[:user_id] = @user.id
 			flash[:success] = 'welcom to aplha blog'
-			redirect_to articles_path
+			redirect_to user_path(@user)
 		
 		else
 			render 'new'
@@ -35,6 +37,13 @@ class UsersController < ApplicationController
 	def index
 		@users = User.paginate(page: params[:page], per_page: 4)
 	end
+	def destroy
+		@user = User.find(params[:id])
+		@user.destroy
+		flash[:success] = "user deleted"
+		redirect_to users_path
+		
+	end
 
 
 	private
@@ -46,10 +55,15 @@ class UsersController < ApplicationController
 		@user = User.find(params[:id])
 	end 
 	def require_same_user
-		if current_user != @user
+		if current_user != @user and !current_user.admin?
 			flash[:danger] = "you are not allowed to do this action"
 			redirect_to root_path
-
+		end
+	end
+	def require_admin 
+		if logged_in? and !current_user.admin?
+			flash[:danger] = "you are not allowed to do this action"
+			redirect_to users_path
 		end
 	end
 
